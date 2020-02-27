0bc3e7dc2c90feb4b12588843f43746fbca34434
Mitul Gautam <mitulgautam@outlook.com>
2020-02-27 10:44:03 +0530

    modif

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
 
-import 'package:flutter_clock_helper/customizer.dart';
-import 'package:flutter_clock_helper/model.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:particle_clock/utils/customizer.dart';
+import 'package:particle_clock/utils/model.dart';
 
 import 'particle_clock.dart';
 
